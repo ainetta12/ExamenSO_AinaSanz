7a063c678f5317876425f944e48f40875ff09320
ainetta12 <ainasanzg@gmail.com>
2024-05-24 09:16:13 +0200

    inventario

---
+++ Assets/Scripts/ScriptableItem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableItem : ScriptableObjects
+{
+
+    public enum item
+    {
+        Item,
+        Armor,
+        Weapon
+    }
+
+    public string ScriptableText;
+    public string Scri
+    public int 
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+}
